cab4346c15adf34e388cbdc46e3021835d3d3d40
ketanmalik <ketanmalik@gmail.com>
2020-08-11 14:16:50 -0500

    user-modal-scaffolding

--- frontend/src/components/AuthUserModal/AuthUserModal.js
+++ frontend/src/components/AuthUserModal/AuthUserModal.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import {
   Button,
@@ -16,40 +16,73 @@ import SignIn from "./SignIn/SignIn";
 import SignUp from "./SignUp/SignUp";
 import "./AuthUserModal.css";
 
-const authUserModal = (props) => {
-  return (
-    <React.Fragment>
-      <Backdrop
-        show={props.showAuthUserModal}
-        clicked={props.onCloseModalHandler}
-      />
-      <div className={`modal ${props.showAuthUserModal ? `open` : `close`}`}>
-        <div className="modal-content">
-          <Segment>
-            <Grid>
-              <Grid.Row>
-                <Grid.Column>
-                  <Header className="modal-content__header">Log In</Header>
-                  <SignIn />
-                </Grid.Column>
-              </Grid.Row>
-              <Divider className="modal-content__desktop-only" horizontal>
-                Or
-              </Divider>
-              <Grid.Row className="modal-content__desktop-only">
-                <Grid.Column>
-                  <Header className="modal-content__header">Sign Up</Header>
-                  <SignUp />
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>
-          </Segment>
+class AuthUserModal extends Component {
+  state = {
+    showSignUpForm: false,
+  };
+
+  backdropClickHandler = () => {
+    this.setState({ showSignUpForm: false }, this.props.onCloseModalHandler);
+  };
+
+  showSignUpFormHandler = () => {
+    this.setState({ showSignUpForm: true });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <Backdrop
+          show={this.props.showAuthUserModal}
+          clicked={this.backdropClickHandler}
+        />
+        <div
+          className={`modal ${this.props.showAuthUserModal ? `open` : `close`}`}
+        >
+          <div className="modal-content">
+            <Segment>
+              <Grid>
+                <Grid.Row>
+                  <Grid.Column>
+                    <Header className="modal-content__header">
+                      {this.state.showSignUpForm ? "Sign Up" : "Log In"}
+                    </Header>
+                    {this.state.showSignUpForm ? (
+                      <SignUp />
+                    ) : (
+                      <React.Fragment>
+                        <div className="modal-content__mobile-only">
+                          New User?
+                          <Button
+                            className="mobile-only-btn"
+                            onClick={this.showSignUpFormHandler}
+                          >
+                            <i>Create an account</i>
+                          </Button>
+                        </div>
+                        <SignIn />
+                      </React.Fragment>
+                    )}
+                  </Grid.Column>
+                </Grid.Row>
+                <Divider className="modal-content__desktop-only" horizontal>
+                  Or
+                </Divider>
+                <Grid.Row className="modal-content__desktop-only">
+                  <Grid.Column>
+                    <Header className="modal-content__header">Sign Up</Header>
+                    <SignUp />
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
+            </Segment>
+          </div>
+          <div className="modal-footer"></div>
         </div>
-        <div className="modal-footer"></div>
-      </div>
-    </React.Fragment>
-  );
-};
+      </React.Fragment>
+    );
+  }
+}
 
 const mapStateToProps = (state) => {
   return {
@@ -63,4 +96,4 @@ const mapDispatchToProps = (dispatch) => {
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(authUserModal);
+export default connect(mapStateToProps, mapDispatchToProps)(AuthUserModal);
